76de1b81d7b4d81e196061c752c1f3de92e7eb39
jebink <jebink7@gmail.com>
2022-11-20 12:21:17 +0530

    feat: import

---
+++ modules/app.py
@@ -0,0 +1,7 @@
+from sales import calc_shipping, calc_tax
+# we have another method to import the sales module
+"""
+That is the import method.
+import sales
+"""
+calc_shipping()
---
+++ modules/sales.py
@@ -0,0 +1,5 @@
+def calc_tax():
+    pass
+def calc_shipping():
+    print("Calculating the shipping")
+    pass
